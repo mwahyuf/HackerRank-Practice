9dd206af1d6a0edbfec51f193d8b4b4d3dabbc66
Muhammad Wahyu Fadli <56909339+mwahyuf@users.noreply.github.com>
2022-08-04 14:36:33 +0700

    Delete Problem Solving directory

--- Problem Solving/simpleArraySum.js
+++
@@ -1,9 +0,0 @@
-function simpleArraySum(ar){
-    let sum = 0;
-    for(let i = 0;i < ar.length; i++){
-        sum += ar[i];
-    }
-    return sum;
-}
-
-console.log("Sum of the array : ", simpleArraySum([1,2,3,4,10,11]))
